195156174ffce426bdd6e398bb2c8ed78072099d
Shirmohammad Tavangari <59179107+Shirmohammad-Ta@users.noreply.github.com>
2025-07-26 03:05:02 +0330

    Add files via upload
    add

--- visualizer.py
+++ visualizer.py
@@ -17,15 +17,16 @@ def plot_history(history):
 
 def describe_emotional_state(H, E):
     if H > 0.7 and E > 0.5:
-        return "😟 Early anxiety – Feeling pressure to be accepted."
+        return "😟\n**Early anxiety** – Feeling pressure to be accepted."
     elif H > 0.4 and E > 0.4:
-        return "😐 Inner conflict – Sacrificing slowly, feeling confused."
+        return "😐\n**Inner conflict** – Sacrificing slowly, feeling confused."
     elif H < 0.3 and E > 0.6:
-        return "😶 Emptiness – Lost identity in exchange for approval."
+        return "😶\n**Emptiness** – Lost identity in exchange for approval."
     elif H > 0.4 and E < 0.3:
-        return "🙂 Balance – Learned to maintain self while connecting."
+        return "🙂\n**Balance** – Learned to maintain self while connecting."
     else:
-        return "🧘 Undefined emotional state."
+        return "🧘\n**Undefined emotional state** – Mixed signals."
+
 
 def draw_emotion_summary(history):
     st.subheader("🧠 Emotional States at Key Time Points")
